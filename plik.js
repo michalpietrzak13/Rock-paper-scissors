017a77a868d4cad4d2916aee3169f95e213f0f78
MagicMajk13 <michalpietrzak385@gmail.com>
2023-08-02 16:00:41 +0200

    AddFunction

--- plik.js
+++ plik.js
@@ -1,63 +1,57 @@
+choices = ['paper', 'rock', 'scissors']
 
-const choices = ["rock","paper","scissors"]
+function computerChoice(){
+  return choices[Math.floor(Math.random()* choices.length)]
+  }
+
+
+
+  function playRound(playerSelection, computerSelection){
+    if (playerSelection === "rock" && computerSelection === "paper") {
+      return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+      return "You Lose! Scissors beat Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+      return "You Lose! Rock beats Scissors";
+  } else {
+      return "You Win! " + playerSelection + " beats " + computerSelection;
+  }
+
+
+
 
 function game() {
 
-}
-
-function playRound(playerSelection, computerSelection){
-  const playerSelection = playerChoice();
-  const computerSelection = computerChoice();
-  const winner = checkWinner(playerSelection,computerSelection)
-  console.log(winner);
-
-}
-const playerChoice = "rock"
-const comp = "paper"
+  let playerScore = 0;
+  let computerScore = 0;
+ 
+  for(let i  = 0; i < 5; i++) {
+    let sign = prompt("Write your choice (paper/rock/scissors)").toLocaleLowerCase();
+    let computerSelection = computerChoice();
+    let result = playRound(sign, computerSelection);
+    console.log(result);
 
 
-function playerChoice(){
-let input = prompt('Type Rock, Paper or Scissors')
-while (input == null) {
-  prompt('Type Rock, Paper or Scissors');
-}
-
-input = input.toLowerCase();
-let check = validateInput(input);
-while (check == false) {
-  input = prompt(
-    "Type Rock, Paper, or Scissors. Spelling needs to be exact, but capitalization doesnt matter");
-}
-  while (input == null) {
-    input = prompt("Type Rock, Paper or Scissors");
+    if (result.startsWith("You Win")){
+      playerScore++;
+    }else if (result.startsWith("You Lose!")){
+      computerScore++;
+    }
     
-  }
-  input = input.toLowerCase();
-  check = validateInput(input);
+    
 }
 
-return input;
+console.log("Final Score:");
+console.log("Player: " + playerScore);
+console.log("Computer: " + computerScore);
 
-function computerChoice(){
-return choices[Math.floor(Math.random()* choices.length)]
-
+if (playerScore > computerScore) {
+  console.log("You won the game!");
+}else if (playerScore < computerScore) {
+  console.log("You Lose");
+}else {
+  console.log("It's Tie");
+ }
 }
-
-function validateInput(choice){
-  return choice.includes(choice);
-   
 }
-function checkWinner(choiceP, ChoiceC){
-  if(choiceP === choiceC){
-    return 'Tie';
-  } else if ((choiceP === "rock" && choiceC === "scissors") ||  
-  (choiceP === "paper" && choiceC === "rock" ||  
-  (choiceP === "scissors" && choiceC === "paper"))){
-    return "Player";
-  } else {
-    return "Computer";
-  }
-
-}
-
-game();
+ 
