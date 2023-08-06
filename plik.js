bba3db811f4fb36c455f034ec44e75b6936e7814
MagicMajk13 <michalpietrzak385@gmail.com>
2023-08-06 18:58:07 +0200

    EndaGame

--- plik.js
+++ plik.js
@@ -1,83 +1,83 @@
-choices = ['paper', 'rock', 'scissors'] // zdefinowanie tablicy
-let playerScore = 0;
-let computerScore = 0;
+const resetbutton = document.getElementById("play-again");
+  resetbutton.addEventListener("click", function () {
+    resetGame();
+  });
 
+  const myButton = document.getElementById("button");
+  myButton.addEventListener("click", function () {
+    game("rock");
+  });
 
-function computerChoice(){
-  return choices[Math.floor(Math.random()* choices.length)] // funkcja ktora zwraca tablice oraz metoda math.random ktora losuje z tablcy wartosc
+  const myButton2 = document.getElementById("button2");
+  myButton2.addEventListener("click", function () {
+    game("paper"); 
+  });
+
+  const myButton3 = document.getElementById("button3");
+  myButton3.addEventListener("click", function () {
+    game("scissors"); 
+  });
+
+  const resultElem = document.querySelector('#result');
+  const scoreElem = document.querySelector('#score'); 
+  const gameResultElem = document.querySelector('#gameResult');
+  const alert = document.querySelector('#alert');
+
+  const choices = ['paper', 'rock', 'scissors'];
+  let playerScore = 0;
+  let computerScore = 0;
+
+  function computerChoice(){
+    return choices[Math.floor(Math.random()* choices.length)];
   }
 
-
-
-  function playRound(playerSelection, computerSelection){ // funkcja ktora rozgrywa runde
+  function playRound(playerSelection, computerSelection){
     if (playerSelection === "rock" && computerSelection === "paper") {
       return "You Lose! Paper beats Rock";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
       return "You Lose! Scissors beat Paper";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
       return "You Lose! Rock beats Scissors";
-
-  }else if (playerSelection === computerSelection){
-    return ("Its a tie");
-  }else {
+    } else if (playerSelection === computerSelection) {
+      return "It's a tie";
+    } else {
       return "You Win! " + playerSelection + " beats " + computerSelection;
- }
-
+    }
   }
 
-
-
-function game(playerSelection) { // glowna funkcja gry
-
+  function game(playerSelection) {
+    if (playerScore === 5 || computerScore === 5) {
+      alert.textContent = "The game has already ended. Please click 'Play Again' to start a new game.";
+    return;
+        }
+    let computerSelection = computerChoice();
+    let result = playRound(playerSelection, computerSelection);
   
-  let computerSelection = computerChoice();// zmienna przypisana do zmiennej computerChoice, ktora zwraca wartosci z tablicy
-  let result = playRound(playerSelection,computerSelection); // zmienna result przypisana do funkcji playRound z parametrami sign i computerSelection
-  
-  
+    if (result.startsWith("You Win")) {
+      playerScore++;
+    } else if (result.startsWith("You Lose!")) {
+      computerScore++;
+    }
 
-  if (result.startsWith("You Win")) {
-    playerScore++;
-  } else if (result.startsWith("You Lose!")) {
-    computerScore++;
+
+    resultElem.textContent = result;
+    scoreElem.textContent = "Player: " + playerScore + ", Computer: " + computerScore;
+
+    if (computerScore === 5) {
+      console.log("Computer wins the game!");
+    } else if (playerScore === 5) {
+      console.log("Player wins the game!");
+      
+    }
+
+    console.log("Final Score:");
+    console.log("Player: " + playerScore);
+    console.log("Computer: " + computerScore);
   }
-  if (playerScore === 5){
-    console.log("You won the game!");
-    resetGame();
+
+  function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    scoreElem.textContent = "Player: " + playerScore + ", Computer: " + computerScore;
+    alert.textContent = ''; 
   }
-
-
-
-  console.log("Final Score:");
-  console.log("Player: " + playerScore);
-  console.log("Computer: " + computerScore);
-
-  
-  
-}
-
-
-      const myButton = document.getElementById("button");
-    myButton.addEventListener("click", function () {
-      game("rock");
-    });
-
-      const myButton2 = document.getElementById("button2");
-    myButton2.addEventListener("click", function () {
-      game("paper"); 
-    });
-
-    const myButton3 = document.getElementById("button3");
-    myButton3.addEventListener("click", function () {
-      game("scissors"); 
-    });
-
-function resetGame(){
-  playerScore = 0;
-  computer
-}  
-    
-
-
-game();
-
- 
